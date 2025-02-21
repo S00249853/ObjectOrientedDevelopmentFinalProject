31d39e59490afaf9ee9629ffd4dc1ec136992ba5
danielpolland <S00249853@atu.ie>
2025-02-21 23:04:23 +0000

    basic class setup. need to add to Game class still, unsure of that it will contain at the moment.

--- Final Project - Daniel Polland/Final Project - Daniel Polland/MainWindow.xaml.cs
+++ Final Project - Daniel Polland/Final Project - Daniel Polland/MainWindow.xaml.cs
@@ -15,9 +15,8 @@ using System.Windows.Shapes;
 
 namespace Final_Project___Daniel_Polland
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
---
+++ Final Project - Daniel Polland/Final Project - Daniel Polland/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project___Daniel_Polland
+{
+    internal class User
+    {
+        string Name { get; set; }
+        string Password { get; set; }
+        List<List<Game>> Lists { get; set; }
+
+        public User(string name, string password) 
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
